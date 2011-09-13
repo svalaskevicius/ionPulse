9acddd392bcacd591cb63a503c387bafc247048a
Sarunas Valaskevicius <sarunas.valaskevicius@gmail.com>
2011-09-13 22:41:33 +0100

    add copyright and license info

---
+++ ionTests/phpparsertest.cpp
@@ -0,0 +1,8 @@
+/*
+  (C) 2011 Sarunas Valaskevicius <rakatan@gmail.com>
+
+  This program is released under the terms of
+  GNU Lesser General Public License version 3.0
+  available at http://www.gnu.org/licenses/lgpl-3.0.txt
+*/
+
