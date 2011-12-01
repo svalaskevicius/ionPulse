52f820d3bc0a3bc4ca5d4798713110a2e3e5e085
Sarunas Valaskevicius <sarunas.valaskevicius@gmail.com>
2011-12-01 21:57:44 +0000

    add missing headers

--- ionParticles/ionPhp/phptreemodel.cpp
+++ ionParticles/ionPhp/phptreemodel.cpp
@@ -1,3 +1,11 @@
+/*
+  (C) 2011 Sarunas Valaskevicius <rakatan@gmail.com>
+
+  This program is released under the terms of
+  GNU Lesser General Public License version 3.0
+  available at http://www.gnu.org/licenses/lgpl-3.0.txt
+*/
+
 #include "phptreemodel.h"
 #include <iostream>
 #include <QFile>
