8a1d7ea2777f7c523f61e3d4ef16f0f11e6b5018
Sarunas Valaskevicius <sarunas.valaskevicius@gmail.com>
2012-02-19 01:58:57 +0000

    update documentation

--- ionCore/main.cpp
+++ ionCore/main.cpp
@@ -7,10 +7,12 @@
 */
 
 #include <QtGui/QApplication>
-#include "mainwindow.h"
-#include "pluginloader.h"
 #include <QMessageBox>
 #include <stdexcept>
+
+#include "shared.h"
+#include "mainwindow.h"
+#include "pluginloader.h"
 
 /**
  * \mainpage Overview
@@ -20,6 +22,25 @@
  * each under their own namespaces.
  *
  * Please see *Namespaces* section for the detailed information.
+ *
+ * To build and develop IonPulse IDE one should install:
+ *   Gcc 4.6.2 or higher, to support C++11 standard;
+ *   Qt 4.8 or higher;
+ *   Boost;
+ *   Bison and Flex;
+ *   Sqlite 3;
+ *   GoogleMock;
+ *
+ *
+ * PLATFORM SPECIFIC instructions:
+ *
+ * MAC OSX
+ * As Apple is not a friend of not-only-for-apple developers, one has to manually
+ * build the required GCC version required.
+ * Please see these links for more instructions:
+ * http://solarianprogrammer.com/2011/12/01/compiling-gcc-4-6-2-on-mac-osx-lion/
+ * http://stackoverflow.com/questions/9102030/g-4-6-adds-a-space-between-f-and-the-path-when-calling-ld-on-mac-os-x/9345746#9345746
+ *
  */
 
 
