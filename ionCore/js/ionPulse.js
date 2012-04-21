2b638488326e8d2e779a48575d94ec201704cb89
Sarunas Valaskevicius <sarunas.valaskevicius@gmail.com>
2012-04-21 22:05:42 +0100

    add js slides highlighter

--- ionCore/js/ionPulse.js
+++ ionCore/js/ionPulse.js
@@ -23,11 +23,59 @@ console.log("ionPulse.js initialised");
 editorPlugin.editorOpened.connect(
     this,
     function (editor) {
-        editor.tabStopWidth = 30;
-        editor.textOptionFlags |= 1; // | 2;// | 4;
+        if (/\.slides$/.test(editor.path)) {
+            editor.setStyleSheet("background-color: #fab976");
+            editor.tabStopWidth = 90;
+        } else {
+            editor.textOptionFlags |= 1; // | 2;// | 4;
+            editor.tabStopWidth = 30;
+        }
     }
 );
 
 
 registerFileHighlighter("text", phpHighlighter);
 registerFileHighlighter("text/php", phpHighlighter);
+
+
+
+
+slideHighlighter = (function () {
+    var _Highlighter = (function () {}).inheritsFrom(TextHighlighterFactory);
+    _Highlighter.prototype.initialize = function () {
+        this.charFormatting = {
+            "text":                 this.createCharFormat(new QColor(100, 80, 60), QFont.Normal, false, 64),
+            "header":               this.createCharFormat(new QColor(120, 60, 40), QFont.Normal, true, 96),
+            "text/black":           this.createCharFormat(new QColor(0, 0, 0), QFont.Normal, true, 64),
+            "text/emphasis":        this.createCharFormat(new QColor(100, 80, 60), QFont.Normal, true, 64),
+            "space":                this.createCharFormat(new QColor(190, 150, 100), QFont.Normal, true, 500),
+        };
+        this.transitions = {
+            "text": {
+                "header": this.regexTransition(/^\./g, true),
+                "space": this.regexTransition(/^___$/g, true),
+            },
+            "header": {
+                "text": this.regexTransition(/$/g, false),
+            },
+            "space": {
+                "text": this.regexTransition(/$/g, false),
+            },
+        };
+        this.highlightRules = {
+            "text" : {
+                "black": /\*[^*]+\*/g,
+                "emphasis": /\/[^\/]+\//g,
+            }
+        };
+        this.parent.initialize.call(this);
+    }
+    var _highlighter = new _Highlighter();
+    _highlighter.initialize();
+    return function (cppApi, text) {
+        _highlighter.highlight(cppApi, text);
+    }
+})();
+
+registerFileType("slides", "text/slide");
+registerFileHighlighter("text/slide", slideHighlighter);
