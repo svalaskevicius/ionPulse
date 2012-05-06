b2bfe6de58b1d5a6cc5bd34fc26756a576079bb1
Sarunas Valaskevicius <sarunas.valaskevicius@gmail.com>
2012-05-06 13:22:55 +0100

    refactoring bracket matcher

--- ionCore/js/_php.js
+++ ionCore/js/_php.js
@@ -21,34 +21,34 @@ phpHighlighter = (function () {
             "php/property":         this.createCharFormat(new QColor(198, 120, 44), QFont.Normal, false),
             "php/function":         this.createCharFormat(new QColor(164, 158, 96), QFont.Normal, false),
             "php/separator":        this.createCharFormat(new QColor(191, 255, 127), QFont.Black, false),
-            "php/comment_sl":          this.createCharFormat(new QColor(100, 100, 100), QFont.Normal, true),
-            "php/comment_ml":          this.createCharFormat(new QColor(100, 100, 100), QFont.Normal, true),
+            "php/comment_sl":       this.createCharFormat(new QColor(100, 100, 100), QFont.Normal, true),
+            "php/comment_ml":       this.createCharFormat(new QColor(100, 100, 100), QFont.Normal, true),
             "php/string_dq":        this.createCharFormat(new QColor(127, 225, 127), QFont.Normal, false),
             "php/string_sq":        this.createCharFormat(new QColor(127, 191, 127), QFont.Normal, false),
         };
         this.transitions = {
             "html": {
-                "php": this.regexTransition(/<\?php\b/g, true)
+                "php":              this.regexTransition(/<\?php\b/g, true)
             },
             "php": {
-                "html": this.regexTransition(/\?>/g, false),
-                "php/comment_ml": this.regexTransition(/\/\*/g, true),
-                "php/string_dq": this.regexTransition(/"/g, true),
-                "php/string_sq": this.regexTransition(/'/g, true),
-                "php/comment_sl": this.regexTransition(/\/\//g, true),
+                "html":             this.regexTransition(/\?>/g, false),
+                "php/comment_ml":   this.regexTransition(/\/\*/g, true),
+                "php/string_dq":    this.regexTransition(/"/g, true),
+                "php/string_sq":    this.regexTransition(/'/g, true),
+                "php/comment_sl":   this.regexTransition(/\/\//g, true),
             },
             "php/comment_ml": {
-                "php": this.regexTransition(/\*\//g, false),
+                "php":              this.regexTransition(/\*\//g, false),
             },
             "php/comment_sl": {
-                "html": this.regexTransition(/\?>/g, false),
-                "php": this.regexTransition(/$/g, false),
+                "html":             this.regexTransition(/\?>/g, false),
+                "php":              this.regexTransition(/$/g, false),
             },
             "php/string_dq": {
-                "php": this.regexTransition(/([^\\"]|\\.|^)"/g, false),
+                "php":              this.regexTransition(/([^\\"]|\\.|^)"/g, false),
             },
             "php/string_sq": {
-                "php": this.regexTransition(/([^\\']|\\.|^)'/g, false),
+                "php":              this.regexTransition(/([^\\']|\\.|^)'/g, false),
             }
         };
         this.highlightRules = {
@@ -81,7 +81,7 @@ phpHighlighter = (function () {
         this.parent.initialize.call(this);
     };
 
-    PhpHighlighter.prototype.addBraketsInfo = function(from, to) {
+    PhpHighlighter.prototype.addBraketsInfo = function (from, to) {
         var blockInfo = [];
         var re = /[\(\)\{\}\[\]]/g;
         re.lastIndex = from;
@@ -91,8 +91,8 @@ phpHighlighter = (function () {
             if (match) {
                 if (match.index < to) {
                     blockInfo.push({
-                        pos : match.index,
-                        char : match[0],
+                        pos: match.index,
+                        char: match[0],
                     });
                 } else {
                     match = false;
@@ -119,143 +119,151 @@ phpHighlighter = (function () {
 
 
 
-var matchingBracketsHighlighter = (function(){
+var matchingBracketsHighlighter = (function () {
 
-    var createSelectionForPos = function(editor, pos) {
-        var selection = new QTextEdit_ExtraSelection();
-        var format = selection.format();
-        var brush = new QBrush();
-        brush.setColor(Qt.black);
-        brush.setStyle(Qt.SolidPattern);
-        //format.setForeground(brush);
-        format.setBackground(brush);
-        selection.setFormat(format);
-        var cursor = editor.textCursor();
-        cursor.setPosition(pos);
-        cursor.movePosition(QTextCursor.NextCharacter, QTextCursor.KeepAnchor);
-        qs.system.setTextEditExtraSelectionCursor(selection, cursor);
-        return selection;
-    };
+    var createSelectionForPos = function (editor, pos) {
+            var selection = new QTextEdit_ExtraSelection();
+            var format = selection.format();
+            var brush = new QBrush();
+            brush.setColor(Qt.black);
+            brush.setStyle(Qt.SolidPattern);
+            //format.setForeground(brush);
+            format.setBackground(brush);
+            selection.setFormat(format);
+            var cursor = editor.textCursor();
+            cursor.setPosition(pos);
+            cursor.movePosition(QTextCursor.NextCharacter, QTextCursor.KeepAnchor);
+            qs.system.setTextEditExtraSelectionCursor(selection, cursor);
+            return selection;
+        };
 
-     var matchingRightBracket = function(character) {
-        switch(character) {
-            case '(': return ')';
-            case '{': return '}';
-            case '[': return ']';
+    var matchingRightBracket = function (character) {
+            switch (character) {
+                case '(': return ')';
+                case '{': return '}';
+                case '[': return ']';
+            }
+            return false;
+        };
+
+    var matchingLeftBracket = function (character) {
+            switch (character) {
+                case ')': return '(';
+                case '}': return '{';
+                case ']': return '[';
+            }
+            return false;
+        };
+
+    var matchLeftBracket = function (currentBlock, i, numLeftBrackets, bracketPair) {
+            var infos = currentBlock.userData();
+            console.log(infos);
+            docPos = currentBlock.position();
+            for (; infos && i < infos.length; ++i) {
+                if (infos[i].char === bracketPair.left) {
+                    ++numLeftBrackets;
+                } else if (infos[i].char === bracketPair.right) {
+                    if (numLeftBrackets === 0) {
+                        return docPos + infos[i].pos;
+                    } else {
+                        --numLeftBrackets;
+                    }
+                }
+            }
+
+            currentBlock = currentBlock.next();
+            if (currentBlock.isValid()) {
+                return matchLeftBracket(currentBlock, 0, numLeftBrackets, bracketPair);
+            }
+
+            return false;
+        }
+
+    var matchRightBracket = function (currentBlock, i, numRightBrackets, bracketPair) {
+            var infos = currentBlock.userData();
+
+            if (i === null && infos) {
+                i = infos.length - 1;
+            }
+            docPos = currentBlock.position();
+            for (; infos && i > -1 && infos.length > 0; --i) {
+                if (infos[i].char === bracketPair.right) {
+                    ++numRightBrackets;
+                } else if (infos[i].char === bracketPair.left) {
+                    if (numRightBrackets === 0) {
+                        return docPos + infos[i].pos;
+                    }--numRightBrackets;
+                }
+            }
+
+            currentBlock = currentBlock.previous();
+            if (currentBlock.isValid()) {
+                return matchRightBracket(currentBlock, null, numRightBrackets, bracketPair);
+            }
+
+            return false;
+        }
+
+    var makeBracketPairFromLeft = function (leftChar) {
+            var rightChar = matchingRightBracket(leftChar);
+            if (rightChar) {
+                return {
+                    left: leftChar,
+                    right: rightChar
+                };
+            }
+            return null;
+        };
+
+    var makeBracketPairFromRight = function (rightChar) {
+            var leftChar = matchingLeftBracket(rightChar);
+            if (leftChar) {
+                return {
+                    left: leftChar,
+                    right: rightChar
+                };
+            }
+            return null;
+        };
+
+    var findMatchingBracket = function(currentChar, startingPos, block) {
+        var bracketPair = makeBracketPairFromLeft(currentChar);
+        if (bracketPair) {
+            var matchedLeft = matchLeftBracket(block, startingPos + 1, 0, bracketPair);
+            if (matchedLeft !== false) {
+                return matchedLeft;
+            }
+        } else {
+            bracketPair = makeBracketPairFromRight(currentChar);
+            if (bracketPair) {
+                var matchedRight = matchRightBracket(block, startingPos - 1, 0, bracketPair);
+                if (matchedRight !== false) {
+                    return matchedRight;
+                }
+           }
         }
         return false;
-     };
-   var matchingLeftBracket = function(character) {
-      switch(character) {
-        case ')': return '(';
-          case '}': return '{';
-          case ']': return '[';
-      }
-      return false;
-   };
+    }
 
-     var matchLeftBracket = function(currentBlock, i, numLeftBrackets, bracketPair)
-     {
-         var infos = currentBlock.userData();
-                                           console.log(infos);
-         docPos = currentBlock.position();
-         for (; infos && i < infos.length; ++i) {
-             if (infos[i].char === bracketPair.left) {
-                 ++numLeftBrackets;
-             } else if (infos[i].char === bracketPair.right) {
-                 if (numLeftBrackets === 0) {
-                     return docPos + infos[i].pos;
-                 } else {
-                     --numLeftBrackets;
-                 }
-             }
-         }
-
-         currentBlock = currentBlock.next();
-         if (currentBlock.isValid()) {
-             return matchLeftBracket(currentBlock, 0, numLeftBrackets, bracketPair);
-         }
-
-         return false;
-     }
-
-     var matchRightBracket = function(currentBlock, i, numRightBrackets, bracketPair)
-     {
-         var infos = currentBlock.userData();
-
-                                           if (i === null && infos) {
-                                               i = infos.length -1;
-                                           }
-         docPos = currentBlock.position();
-         for (; infos && i > -1 && infos.length > 0; --i) {
-             if (infos[i].char === bracketPair.right) {
-                 ++numRightBrackets;
-             } else if (infos[i].char === bracketPair.left) {
-                 if (numRightBrackets === 0) {
-                     return docPos + infos[i].pos;
-                 }
-                 --numRightBrackets;
-             }
-         }
-
-         currentBlock = currentBlock.previous();
-         if (currentBlock.isValid()) {
-             return matchRightBracket(currentBlock, null, numRightBrackets, bracketPair);
-         }
-
-         return false;
-     }
-
-                                       var makeBracketPairFromLeft = function(leftChar) {
-                                           var rightChar = matchingRightBracket(leftChar);
-                                           if (rightChar) {
-                                               return {
-                                                   left: leftChar,
-                                                   right: rightChar
-                                               };
-                                           }
-                                           return null;
-                                       };
-                                       var makeBracketPairFromRight = function(rightChar) {
-                                           var leftChar = matchingLeftBracket(rightChar);
-                                           if (leftChar) {
-                                               return {
-                                                   left: leftChar,
-                                                   right: rightChar
-                                               };
-                                           }
-                                           return null;
-                                       };
-
-    return function(editor) {
-        try{
+    return function (editor) {
+        try {
             var selections = editor.extraSelections();
             var textCursor = editor.textCursor();
             var userData = textCursor.block().userData();
             var curPos = textCursor.position() - textCursor.block().position();
             if (userData) {
-                for (var i = userData.length-1; i>=0; --i) {
+                for (var i = userData.length - 1; i >= 0; --i) {
                     if (userData[i].pos == (curPos - 1)) {
-                        var bracketPair = makeBracketPairFromLeft(userData[i].char);
-                        if (bracketPair) {
-                            var matchedLeft = matchLeftBracket(textCursor.block(), i + 1, 0, bracketPair);
-                            console.log(matchedLeft);
-                            if (matchedLeft !== false) {
-                                selections.push(createSelectionForPos(editor, matchedLeft));
-                                selections.push(createSelectionForPos(editor, textCursor.block().position() + userData[i].pos));
-                            }
-                        } else if (bracketPair = makeBracketPairFromRight(userData[i].char)) {
-                            var matchedRight = matchRightBracket(textCursor.block(), i - 1, 0, bracketPair);
-                            if (matchedRight !== false) {
-                                selections.push(createSelectionForPos(editor, matchedRight));
-                                selections.push(createSelectionForPos(editor, textCursor.block().position() + userData[i].pos));
-                            }
+                        var matchedPos = findMatchingBracket(userData[i].char, i, textCursor.block());
+                        if (false !== matchedPos) {
+                            selections.push(createSelectionForPos(editor, matchedPos));
+                            selections.push(createSelectionForPos(editor, textCursor.block().position() + userData[i].pos));
                         }
                     }
                 }
             }
             editor.setExtraSelections(selections);
-        } catch(e) {
+        } catch (e) {
             console.error(e);
         }
     }
@@ -263,10 +271,8 @@ var matchingBracketsHighlighter = (function(){
 
 
 editorPlugin.editorOpened.connect(
-    this,
-    function (editor) {
-        editor.cursorPositionChanged.connect(this, function(){
-            matchingBracketsHighlighter(editor);
-        });
-    }
-);
+this, function (editor) {
+    editor.cursorPositionChanged.connect(this, function () {
+        matchingBracketsHighlighter(editor);
+    });
+});
